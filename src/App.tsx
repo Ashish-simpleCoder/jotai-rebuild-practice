8a0f762c19879c1d66f53575947b5999aa541a41
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2023-06-21 16:24:39 +0530

    Add computed atom and setter function
    - Add computed atom
    - Add setter function implementation which works like the setter function returned from useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { atom, useAtom, useAtomValue } from './jotai'
 
 const salaryAtom = atom(100_000)
+const bonusAtom = atom(100_0)
+const totalSalaryAtom = atom((get) => get(salaryAtom) + get(bonusAtom))
+const userAtom = atom({ name: 'Coder', age: 1000 })
 
 export default function App() {
    const [salary, setSalary] = useAtom(salaryAtom)
+   const [bonus, setBonus] = useAtom(bonusAtom)
+   const totalSalary = useAtomValue(totalSalaryAtom)
 
    return (
       <>
@@ -11,10 +16,18 @@ export default function App() {
             <div>
                <input value={salary} onChange={(e) => setSalary(+e.target.value)} />
             </div>
-            <div>
-               <p>Salary : {salary}</p>
-            </div>
             <DisplaySalary />
+
+            <hr />
+
+            <div>
+               <input value={bonus} onChange={(e) => setBonus(+e.target.value)} />
+            </div>
+            <DisplayBonus />
+            {totalSalary}
+
+            <hr />
+            <User />
          </div>
       </>
    )
@@ -23,4 +36,36 @@ export default function App() {
 function DisplaySalary() {
    const salary = useAtomValue(salaryAtom)
    return <div>Salary: {salary}</div>
+}
+function DisplayBonus() {
+   const bonus = useAtomValue(bonusAtom)
+   return <div>Salary: {bonus}</div>
+}
+function User() {
+   return (
+      <>
+         <h3>User Atom</h3>
+         <Name />
+         <br />
+         <Age />
+      </>
+   )
+}
+
+function Name() {
+   const [{ name }, setUser] = useAtom(userAtom)
+
+   return (
+      <>
+         <div>
+            <input value={name} onChange={(e) => setUser((s) => ({ ...s, name: e.target.value }))} />
+         </div>
+         <div>name: {name}</div>
+      </>
+   )
+}
+function Age() {
+   const age = useAtomValue(userAtom).age
+
+   return <div>age: {age}</div>
 }
--- src/jotai/index.tsx
+++ src/jotai/index.tsx
@@ -5,10 +5,27 @@ interface Atom<AtomType> {
    set: (newValue: AtomType) => void
    subscribe: (callback: (newValue: AtomType) => void) => () => void
 }
+type AtomGetter<AtomType> = (get: <TargetAtom>(a: Atom<TargetAtom>) => TargetAtom) => AtomType
+type DispatchAtom<AtomType> = (cb: AtomType | ((a: AtomType) => AtomType)) => void
 
-export function atom<AtomType>(initialValue: AtomType): Atom<AtomType> {
-   let value = initialValue
+export function atom<AtomType>(initialValue: AtomType | AtomGetter<AtomType>): Atom<AtomType> {
+   let value = typeof initialValue == 'function' ? (null as AtomType) : initialValue
    const subscribers = new Set<(newValue: AtomType) => void>()
+
+   const get = <Target,>(atom: Atom<Target>) => {
+      let currentValue = atom.get()
+      atom.subscribe((newValue) => {
+         if (currentValue === newValue) return
+         currentValue = newValue
+         computeValue()
+         subscribers.forEach((cb) => cb(value))
+      })
+      return currentValue
+   }
+   const computeValue = () => {
+      value = typeof initialValue == 'function' ? (initialValue as AtomGetter<AtomType>)(get) : initialValue
+   }
+   computeValue()
 
    return {
       get: () => value,
@@ -23,8 +40,16 @@ export function atom<AtomType>(initialValue: AtomType): Atom<AtomType> {
    }
 }
 
-export function useAtom<AtomType>(atom: Atom<AtomType>): [AtomType, (newValue: AtomType) => void] {
-   return [useSyncExternalStore(atom.subscribe, atom.get), atom.set]
+export function useAtom<AtomType>(atom: Atom<AtomType>): [AtomType, DispatchAtom<AtomType>] {
+   const setter: DispatchAtom<AtomType> = (cb) => {
+      if (typeof cb == 'function') {
+         const val = (cb as (a: AtomType) => AtomType)(atom.get())
+         atom.set(val)
+      } else {
+         atom.set(cb)
+      }
+   }
+   return [useSyncExternalStore(atom.subscribe, atom.get), setter]
 }
 export function useAtomValue<AtomType>(atom: Atom<AtomType>): AtomType {
    return useSyncExternalStore(atom.subscribe, atom.get)
