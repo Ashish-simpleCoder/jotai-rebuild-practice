16e99039fc8fa5db6e3c4c2bb22714e7d57480de
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2023-06-21 12:27:04 +0530

    [Add] atom, useAtom and useAtomValue functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-import { atom, useAtom } from './jotai'
+import { atom, useAtom, useAtomValue } from './jotai'
+
+const salaryAtom = atom(100_000)
 
 export default function App() {
-   return <></>
+   const [salary, setSalary] = useAtom(salaryAtom)
+
+   return (
+      <>
+         <div>
+            <div>
+               <input value={salary} onChange={(e) => setSalary(+e.target.value)} />
+            </div>
+            <div>
+               <p>Salary : {salary}</p>
+            </div>
+            <DisplaySalary />
+         </div>
+      </>
+   )
+}
+
+function DisplaySalary() {
+   const salary = useAtomValue(salaryAtom)
+   return <div>Salary: {salary}</div>
 }
--- src/jotai/index.tsx
+++ src/jotai/index.tsx
@@ -1,3 +1,31 @@
-export function atom() {}
+import { useSyncExternalStore } from 'react'
 
-export function useAtom() {}
+interface Atom<AtomType> {
+   get: () => AtomType
+   set: (newValue: AtomType) => void
+   subscribe: (callback: (newValue: AtomType) => void) => () => void
+}
+
+export function atom<AtomType>(initialValue: AtomType): Atom<AtomType> {
+   let value = initialValue
+   const subscribers = new Set<(newValue: AtomType) => void>()
+
+   return {
+      get: () => value,
+      set: (newValue) => {
+         value = newValue
+         subscribers.forEach((callback) => callback(value))
+      },
+      subscribe: (callback) => {
+         subscribers.add(callback)
+         return () => subscribers.delete(callback)
+      },
+   }
+}
+
+export function useAtom<AtomType>(atom: Atom<AtomType>): [AtomType, (newValue: AtomType) => void] {
+   return [useSyncExternalStore(atom.subscribe, atom.get), atom.set]
+}
+export function useAtomValue<AtomType>(atom: Atom<AtomType>): AtomType {
+   return useSyncExternalStore(atom.subscribe, atom.get)
+}
